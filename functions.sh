26d55f529b4198d493c5edd861af98e37ba8d88b
[a551894] Pierre Quételart <pierre.quetelart@atos.net>
2017-01-10 08:54:07 +0100

    Blink gpio

--- functions.sh
+++ functions.sh
@@ -7,10 +7,40 @@
 # XX is a short code for your plugin, ex: ww for Weather Wunderground
 # You can use translations provided in the language folders functions.sh
 jv_pg_gpio_action () {
-    for key in "${!jv_pg_gpio_pin[@]}"
+    key=-1
+    for pin in "${!jv_pg_gpio_pin[@]}"
     do 
-        if [[ $order == *${jv_pg_gpio_pin[$key]}* ]]; then
-            gpio mode $key out && gpio write $key $1;
+        if [ "$order" == *${jv_pg_gpio_pin[$pin]}* ]; then
+            key=$pin
+            break
+            ;;
         fi
     done
+    if [ $key -gt 0 ]; then
+        /usr/bin/touch "stop_$key"
+        case $1 in
+            on)
+                gpio mode $key out && gpio write $key 1
+            off)
+                gpio mode $key out && gpio write $key 0
+            blink)
+                while true
+                do
+                    gpio mode $key out && gpio write $key 1
+                    sleep(1)
+                    gpio mode $key out && gpio write $key 1
+                    sleep(1)
+                    
+                    if [ -e "stop_$key" ]; then
+                        break
+                        ;;
+                    fi
+                done
+        esac
+        /bin/rm "stop_$key"
+        return 1
+    else
+        return 0
+    fi
+
 }
