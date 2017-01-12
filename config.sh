7c7681422a05660dc2b6784265df162cc862e63e
Unknown <batmantl1@gmail.com>
2017-01-12 18:57:38 +0100

    Real working case

--- config.sh
+++ config.sh
@@ -1,3 +1,1 @@
-jv_pg_gpio_pin[4]="horloge"
-jv_pg_gpio_pin[5]="cœur"
-jv_pg_gpio_pin[6]="lumière"
+jv_pg_gpio_pin[4]='cœur'; jv_pg_gpio_pin[5]='horloge'; jv_pg_gpio_pin[6]='lumière'
--- functions.sh
+++ functions.sh
@@ -1,50 +1,42 @@
 #!/bin/bash
-# Here you can create functions which will be available from the commands file
-# You can also use here user variables defined in your config file
-# To avoid conflicts, name your function like this
-# pg_XX_myfunction () { }
-# pg for PluGin
-# XX is a short code for your plugin, ex: ww for Weather Wunderground
-# You can use translations provided in the language folders functions.sh
 jv_pg_gpio_action () {
     key=-1
+    order=${order:-$2}
+
     for pin in "${!jv_pg_gpio_pin[@]}"
-    do 
-        if [ "$order" == *${jv_pg_gpio_pin[$pin]}* ]; then
+    do
+        device=${jv_pg_gpio_pin[pin]}
+        regex="^.*${device}.*$"
+        if [[ $order =~ $regex ]]; then
             key=$pin
             break
         fi
     done
+
     if [ $key -gt 0 ]
     then
-        /usr/bin/touch "stop_$key"
+        if [ -e "stop_$key" ]
+        then
+            /bin/rm "stop_$key"
+        fi
         case $1 in
             on)
+                /usr/bin/touch "stop_$key"
                 gpio mode $key out && gpio write $key 1
                 ;;
             off)
+                /usr/bin/touch "stop_$key"
                 gpio mode $key out && gpio write $key 0
                 ;;
             blink)
                 gpio mode $key out
-                while true
-                do
-                    gpio write $key 1
-                    sleep 1
-                    gpio write $key 0
-                    sleep 1
-                    
-                    if [ -e "stop_$key" ]
-                    then
-                        break
-                    fi
-                done
-                ;;
-        esac
-        /bin/rm "stop_$key"
-        return 1
-    else
-        return 0
-    fi
 
+                # Must use a detach thread to end this command
+                parallel "while true; do gpio write $key 1 && sleep 2 && gpio write $key 0 && sleep 2; if [ -e \"stop_$key\" ]; then break; fi; done"
+		;;
+        esac
+        return 0
+    else
+        return 1
+    fi
 }
